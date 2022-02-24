42ea4f85fc3073965359af1b40ee4e3677d230c1
vflo <v.flo@creaf.uab.cat>
2022-02-24 17:59:38 +0000

    calc_h2o_prop modification

--- R/hyd_transpiration.R
+++ R/hyd_transpiration.R
@@ -36,19 +36,28 @@ integral_P_approx = function(dpsi, psi_soil, psi50, b, ...){
   -dpsi * P(psi_soil-dpsi/2, psi50, b)
 }
 
-## Calculation of water properties
-calc_h2O_prop <- function(tc, p){
-  R = 287.058 #Specific gas constant for dry air J kg-1 K-1
+## Calculation of water parameters
+calc_h2O_params <- function(tc, p){
+  R =  8.31446261815324 #universal gas constant m3 Pa K−1 mol−1
+  dry_air_mol = 0.0289647 #kg mol-1
+  R_dry_air = R/dry_air_mol # specific dry air gas constant J kg-1 K-1
+  h2o_mol_mass = 0.01801528
+  R_water = R/h2o_mol_mass # specific water vapour gas constant J kg-1 K-1
   # dry air density 
-  air_dens = p/(R*(tc+273.15)) #kg m-3
+  air_dens = p/(R_dry_air*(tc+273.15)) #kg m-3
   # dry air specific heat capacity at constant pressure
   cp = 1012 #J kg-1 K-1
   # Latent heat of water vaporization
-  L =  2500.8-2.36*Tc+0.0016*tc^2-0.00006*tc^3 #J kg-1
-  # Psychrometric constant
-  psichro = (p*cp)/(0.622*L) #Pa K-1
-  #H2O mol per Kg
-  C = 18 #mol kg-1
+  L =  2500.8-2.36*tc+0.0016*tc^2-0.00006*tc^3 #J kg-1
+  # psychrometric constant
+  pch = (p*cp)/(0.622*L) #Pa K-1
+  # H2O mol Kg-1
+  C = 1/h2o_mol_mass #mol kg-1
+  # slope of the curve relating saturation vapour pressure to temperature
+  S = 4098*(0.6108*exp(17.27*tc/(tc+237.3)))/((237.3+tc)^2) *1000 #FAO Pa K-1
+  
+  df = data.frame(R, dry_air_mol, R_dry_air, air_dens, cp, L, pch)
+  return(df)
 }
 
 ## Calculation of aerodynamic conductance
@@ -58,7 +67,7 @@ calc_ga <- function(u,ustar){
   if(is.na(ustar)){
     1/((u/ustar)+135*ustar^(-0.67)) #Thom 1972 Momentum, mass and heat exchange of vegetation
   }else{
-    u/208
+    u/208 #Allen et al 1998 crop evapotranspiration-guidelines for computing crop water requirements
   }
 }
 
@@ -72,19 +81,22 @@ calc_ga <- function(u,ustar){
 #' @param par_plant numeric, A list of plant hydraulic parameters, which must include ...... 
 #' @param par_env numeric, A list of environmental parameters
 #'
-#' @return numeric, stomatal conductance in mol/m2/s
+#' @return numeric, canopy stomatal conductance in mol/m2/s
 #'
 #'
 #' @export
 #'
-calc_gs = function(dpsi, psi_soil, par_plant, par_env, ...){
+calc_gs_PM = function(dpsi, psi_soil, par_plant, par_env, ...){
   K = scale_conductivity(par_plant$conductivity, par_env)
   D = (par_env$vpd/par_env$patm)
+  h2o_params = calc_h2O_params(par_env$tc,par_env$patm)
+  R = h2o_params$R
+  dens = h2o_params$air_dens
+  cp =h2o_params$cp
+  L = h2o_params$L
+  pch = h2o_params$pch
+  ga = calc_ga(par_env$u, par_env$ustar)
   K/1.6/D * -integral_P(dpsi, psi_soil, par_plant$psi50, par_plant$b, ...)
-  # # papprox = P(psi_soil-dpsi/2, par_plant$psi50, par_plant$b)
-  # papprox = P(psi_soil, par_plant$psi50, par_plant$b)-Pprime(psi_soil, par_plant$psi50, par_plant$b)*dpsi/2.5
-  # K/1.6/D * dpsi * papprox
-  
 }
 
 #' Stomatal conductance and Transpiration
