0dea33cc9aaec33d459a7d9d7bcbd915c88fd03a
vflo <v.flo@creaf.uab.cat>
2022-02-24 15:47:38 +0000

    Water properties and aerodynamic conductance functions

--- R/hyd_transpiration.R
+++ R/hyd_transpiration.R
@@ -36,6 +36,56 @@ integral_P_approx = function(dpsi, psi_soil, psi50, b, ...){
   -dpsi * P(psi_soil-dpsi/2, psi50, b)
 }
 
+## Calculation of water properties
+calc_h2O_prop <- function(tc, p){
+  R = 287.058 #Specific gas constant for dry air J kg-1 K-1
+  # dry air density 
+  air_dens = p/(R*(tc+273.15)) #kg m-3
+  # dry air specific heat capacity at constant pressure
+  cp = 1012 #J kg-1 K-1
+  # Latent heat of water vaporization
+  L =  2500.8-2.36*Tc+0.0016*tc^2-0.00006*tc^3 #J kg-1
+  # Psychrometric constant
+  psichro = (p*cp)/(0.622*L) #Pa K-1
+  #H2O mol per Kg
+  C = 18 #mol kg-1
+}
+
+## Calculation of aerodynamic conductance
+calc_ga <- function(u,ustar){
+  # u = wind speed m s-1
+  # ustar = friction velocity m s-1
+  if(is.na(ustar)){
+    1/((u/ustar)+135*ustar^(-0.67)) #Thom 1972 Momentum, mass and heat exchange of vegetation
+  }else{
+    u/208
+  }
+}
+
+#' Stomatal conductance and Transpiration using PM extension
+#'
+#' Calculates regulated stomatal conducatnce given the leaf water potential, 
+#' plant hydraulic traits, the environment.
+#'
+#' @param dpsi numeric, soil-to-leaf water potential difference (\eqn{\psi_s-\psi_l}), Mpa
+#' @param psi_soil numeric, soil water potential, Mpa
+#' @param par_plant numeric, A list of plant hydraulic parameters, which must include ...... 
+#' @param par_env numeric, A list of environmental parameters
+#'
+#' @return numeric, stomatal conductance in mol/m2/s
+#'
+#'
+#' @export
+#'
+calc_gs = function(dpsi, psi_soil, par_plant, par_env, ...){
+  K = scale_conductivity(par_plant$conductivity, par_env)
+  D = (par_env$vpd/par_env$patm)
+  K/1.6/D * -integral_P(dpsi, psi_soil, par_plant$psi50, par_plant$b, ...)
+  # # papprox = P(psi_soil-dpsi/2, par_plant$psi50, par_plant$b)
+  # papprox = P(psi_soil, par_plant$psi50, par_plant$b)-Pprime(psi_soil, par_plant$psi50, par_plant$b)*dpsi/2.5
+  # K/1.6/D * dpsi * papprox
+  
+}
 
 #' Stomatal conductance and Transpiration
 #'
